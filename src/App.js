34b267f24e1d6b92a51dcf2ccb954d95e666e237
shahid <princesamofficial@gmail.com>
2023-05-18 14:34:03 +0530

    json showing in console but not rendering on page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+
+export default function App() {
+
+  const [data, setData] = useState([]);
+
+
+  // Fetch Movies Data from JSON file
+  const getData = () =>{
+    fetch('./Data/json/data.json', {headers: {
+      'Content-Type' :'application/json',
+      'Accept' : 'application/json'
+    }}).then((response)=>{
+      return response.json()
+    }).then((myjson)=>{
+      setData(myjson);
+      console.log(myjson);
+    })
+  }
+  
+  
+  useEffect (()=>{
+    getData()
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className = "movie-data">
+        {
 
-export default App;
+          data && data.length > 0 && data.map((movies)=> {
+            return (
+              <h1 key={movies.id}>{movies.title}</h1>
+            )
+          })
+        }
+      </div>
+  );
+
+  };
+
+
+
+
+// export default App;
+
+
+
+
+
+// const [movies, setMovies] = useState([]);
+  
+// useEffect(() => {
+//   const fetchMovies = async () => {
+//     try {
+//       const response = await fetch("./Data/json/data.json");
+//       const data = await response.json();
+//       setMovies(data.movies);
+//     } catch (error) {
+//       console.error("Error fetching movies:", error);
+//     }
+//   };
+
+//   fetchMovies();
+// }, []);
+
+// return (
+//   <div>
+//     {movies.map((movie) => (
+//       <div key={movie.id}>
+//         <h2>{movie.title}</h2>
+//         <p>Year: {movie.year}</p>
+//         <p>Genres: {movie.genres.join(", ")}</p>
+//         <p>Ratings: {movie.ratings.join(", ")}</p>
+//         <img src={movie.posterurl} alt={movie.title} />
+//         <p>Content Rating: {movie.contentRating}</p>
+//         <p>Duration: {movie.duration}</p>
+//         <p>Release Date: {movie.releaseDate}</p>
+//         <p>Storyline: {movie.storyline}</p>
+//         <p>Actors: {movie.actors.join(", ")}</p>
+//       </div>
+//     ))}
+//   </div>
+// );
